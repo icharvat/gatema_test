63fb7d409bc8ff371d6977b65fd9e35ff9e31ee2
icharvat <91603889+icharvat@users.noreply.github.com>
2021-09-29 13:27:48 +0200

    Add files via upload

--- zmenahodnot.py
+++ zmenahodnot.py
@@ -4,22 +4,18 @@ filename = "D327971_fc1.i"
 
 with open(filename, "r") as f:
     loaded = f.readlines()
-    
-    #zapsání upravených souřadnic do jiného souboru
-    with open("test3.txt", "w") as g:
-            for i in loaded:
-                #rozdělení textu od čísel
-                try:
-                    x, y = i.split("X")[1].split("Y")
-                    x = float(x)
-                    y = float(y)
-                    #přidání hodnoty k Y, pokud je x>50
-                    if x > 50:
-                        y = y +10
-                    x = str(x)
-                    y = str(y)
-                    g.write(x + y)
-                #ignorování prvních pár řádků bez souřadnic
-                except IndexError:
-                        pass
+    for i in loaded:
+        #rozdělení textu od čísel
+        try:
+            x, y = i.split("X")[1].split("Y")
+            x = float(x)
+            y = float(y)
+            #přidání hodnoty k Y, pokud je x>50
+            if x > 50:
+                y = y +10
+            x = str(x)
+            y = str(y)
+            #ignorování prvních pár řádků bez souřadnic
+        except IndexError:
+            pass
 
